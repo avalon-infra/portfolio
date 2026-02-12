4432f163187bab1b393625f2f9e4a052b660ea61
Matiix310 <lucas.stephan.12@outlook.fr>
2026-02-12 12:42:39 +0100

    feat(lint): setup eslint and husky

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
-import { animate, stagger, inView, ElementOrSelector } from "motion";
+import { animate, stagger, inView, ElementOrSelector, MotionKeyframesDefinition } from 'motion';
+import logger from './logger';
 
-function fadeInAnimation(elements: ElementOrSelector) {
+const fadeInAnimation = (elements: ElementOrSelector): void => {
   inView(
     elements,
     ({ target }) => {
-      animate(
-        target,
-        { opacity: 1, transform: "none" },
-        { delay: 0.2, duration: 0.7, easing: "ease-in-out" }
-      );
+      animate(target, { opacity: 1, transform: 'none' }, { delay: 0.2, duration: 0.7, easing: 'ease-in-out' });
     },
-    { margin: "-20px 0px -20px 0px" }
+    { margin: '-20px 0px -20px 0px' }
   );
-}
+};
 
-function runCommonAnimations() {
-  const sections = [...document.getElementsByTagName("section")];
-  const topBar = document.getElementById("topBarMenu")!;
-  const topBarItems = [...topBar.getElementsByTagName("a")];
+const runCommonAnimations = (): void => {
+  const sections = [...document.getElementsByTagName('section')];
+  const topBar = document.getElementById('topBarMenu')!;
+  const topBarItems = [...topBar.getElementsByTagName('a')];
 
   const observerOptions: IntersectionObserverInit = {
     threshold: 0.7,
@@ -29,10 +26,10 @@ function runCommonAnimations() {
     const index = sections.findIndex((s) => s.id == intersectedSections[0].target.id) - 1;
     // reset
     topBarItems.forEach((v) => {
-      v.classList.remove("active");
+      v.classList.remove('active');
     });
     if (index >= 0 && index < topBarItems.length) {
-      topBarItems[index].classList.add("active");
+      topBarItems.find((_, i) => i === index)?.classList.add('active');
     }
   }, observerOptions);
 
@@ -41,42 +38,41 @@ function runCommonAnimations() {
   });
 
   // animate section titles
-  const sectionTitles = [...document.getElementsByClassName("section-title")];
+  const sectionTitles = [...document.getElementsByClassName('section-title')];
   fadeInAnimation(sectionTitles);
 
   // animate top bar
-  fadeInAnimation(document.getElementById("topBar")!);
-}
+  fadeInAnimation(document.getElementById('topBar')!);
+};
 
-function runHomeSection() {
+const runHomeSection = (): void => {
   // Mouse animation
-  const section1Mouse = document.getElementById("section1Mouse");
+  const section1Mouse = document.getElementById('section1Mouse');
   if (!section1Mouse) {
-    console.error("#section1Mouse can't be found in the DOM");
+    logger.error("#section1Mouse can't be found in the DOM");
     return;
   }
 
-  const homeSection = document.getElementById("home");
+  const homeSection = document.getElementById('home');
   if (!homeSection) {
-    console.error("#home can't be found in the DOM");
+    logger.error("#home can't be found in the DOM");
     return;
   }
 
-  const resizeSection1Mouse = () => {
+  const resizeSection1Mouse = (): void => {
     const maxWidth = window.innerWidth * 3;
     const maxHeight = window.innerHeight * 3;
     const size = maxHeight > maxWidth ? maxWidth : maxHeight;
 
-    section1Mouse.style.maskSize = size + "px " + size + "px";
+    section1Mouse.style.maskSize = `${size}px ${size}px`;
   };
 
-  homeSection.addEventListener("mousemove", (e) => {
-    const halfSize = parseInt(section1Mouse.style.maskSize.split("px")[0]) / 2;
-    section1Mouse.style.maskPosition =
-      e.clientX - halfSize + "px " + (e.clientY - halfSize) + "px";
+  homeSection.addEventListener('mousemove', (e) => {
+    const halfSize = parseInt(section1Mouse.style.maskSize.split('px')[0]) / 2;
+    section1Mouse.style.maskPosition = `${e.clientX - halfSize}px ${e.clientY - halfSize}px`;
   });
 
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     resizeSection1Mouse();
   });
 
@@ -84,56 +80,48 @@ function runHomeSection() {
 
   // Matiix310 animation
 
-  const drawStroke = (p: number) => ({
+  const drawStroke = (p: number): MotionKeyframesDefinition => ({
     strokeDashoffset: 1 - p,
-    visibility: "visible",
+    visibility: 'visible',
   });
 
-  animate("#home .title path", drawStroke(1), { delay: stagger(0.1) });
-  animate(
-    "#home .title path",
-    { fill: "white" },
-    { delay: stagger(0.1, { start: 0.2 }) }
-  );
-}
+  animate('#home .title path', drawStroke(1), { delay: stagger(0.1) });
+  animate('#home .title path', { fill: 'white' }, { delay: stagger(0.1, { start: 0.2 }) });
+};
 
-function runAboutSection() {
-  const experienceContainers = [
-    ...document.getElementsByClassName("experience-container"),
-  ];
+const runAboutSection = (): void => {
+  const experienceContainers = [...document.getElementsByClassName('experience-container')];
 
   fadeInAnimation(experienceContainers);
-}
+};
 
-function runProjectsSection() {
-  const projectsSection = document.getElementById("projects");
+const runProjectsSection = (): void => {
+  const projectsSection = document.getElementById('projects');
   if (!projectsSection) {
-    console.error("#projects can't be found in the DOM");
+    logger.error("#projects can't be found in the DOM");
     return;
   }
 
-  const projects = [
-    ...projectsSection.getElementsByClassName("project"),
-  ] as HTMLAnchorElement[];
+  const projects = [...projectsSection.getElementsByClassName('project')] as HTMLAnchorElement[];
 
-  projectsSection.addEventListener("mousemove", (e) => {
+  projectsSection.addEventListener('mousemove', (e) => {
     const x = e.clientX;
     const y = e.clientY;
 
-    for (let project of projects) {
+    for (const project of projects) {
       const boundingRect = project.getBoundingClientRect();
-      project.style.setProperty("--x", (x - boundingRect.left).toString() + "px");
-      project.style.setProperty("--y", (y - boundingRect.top).toString() + "px");
+      project.style.setProperty('--x', `${(x - boundingRect.left).toString()}px`);
+      project.style.setProperty('--y', `${(y - boundingRect.top).toString()}px`);
     }
   });
 
   fadeInAnimation(projects);
-}
+};
 
-function runTechLanguagesSection() {
-  const techs = [...document.querySelectorAll("#techLanguages .languages div")];
+const runTechLanguagesSection = (): void => {
+  const techs = [...document.querySelectorAll('#techLanguages .languages div')];
   fadeInAnimation(techs);
-}
+};
 
 // INIT
 runCommonAnimations();
